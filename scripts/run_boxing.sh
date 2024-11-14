5bc635c01b74054223acd5974ab02d5b841f981f
zhouzt21 <zhou-zt21@mails.tsinghua.edu.cn>
2024-11-14 21:38:15 +0800

    add run_boxing

--- scripts/run_boxing.sh
+++ scripts/run_boxing.sh
@@ -4,5 +4,3 @@ python phc/run_hydra.py     project_name=SMPLOlympics num_agents=2     \
         env=env_amp_z env.num_envs=2048 env.task=HumanoidBoxingZ env.enableTaskObs=True  \
         env.stateInit=Start    robot=smpl_humanoid_boxing +env.models=["output/HumanoidIm/pulse_vae/Humanoid.pth"]      \
         env.motion_file=./sample_data/video_boxing_afterproc_upright.pkl headless=True env.episode_length=300  learning.params.config.switch_frequency=250
-
-
